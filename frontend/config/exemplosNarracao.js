1506161c3e77f0b61521fcb21d603d70c7afba91
Torres <torrestecnologiaia@gmail.com>
2026-05-13 19:27:38 -0300

    Add frontend/config/exemplosNarracao.js

---
+++ frontend/config/exemplosNarracao.js
@@ -0,0 +1,24 @@
+// config/exemplosNarracao.js
+export const exemplosNarracao = {
+  // Narração de gol
+  golDoBrasil: {
+    text: "GOOOOOOL! Neymar faz um golaço de falta aos 45 minutos do segundo tempo!",
+    voice: "galvao-bueno",
+    emotion: "extrema_emocao"
+  },
+  
+  // Abertura de programa
+  aberturaPrograma: {
+    text: "Boa noite, Brasil! Hoje vamos transformar vidas com histórias emocionantes!",
+    voice: "luciano-huck",
+    emotion: "motivacional"
+  },
+  
+  // Momento engraçado
+  momentoHumor: {
+    text: "É mentira, Terta? O Brasil hoje tá muito engraçado!",
+    voice: "chico-anysio",
+    emotion: "humor",
+    character: "alberto_roberto"
+  }
+};
