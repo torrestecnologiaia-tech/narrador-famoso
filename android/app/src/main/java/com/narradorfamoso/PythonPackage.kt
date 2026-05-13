849f2363bfd23c6b2a2c51f0b759878138e35499
Torres <torrestecnologiaia@gmail.com>
2026-05-13 19:27:33 -0300

    Add android/app/src/main/java/com/narradorfamoso/PythonPackage.kt

---
+++ android/app/src/main/java/com/narradorfamoso/PythonPackage.kt
@@ -0,0 +1,16 @@
+package com.narradorfamoso
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class PythonPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(PythonModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
